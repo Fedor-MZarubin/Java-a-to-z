4cecd800c1ce1ebba2875d9af04e74a1d832bbda
Fedor-MZarubin <hard88@inbox.ru>
2017-01-14 22:07:05 +0300

    Java-a-to-z

--- chapter_001/src/main/java/ru/zarubin/Calculate.java
+++ chapter_001/src/main/java/ru/zarubin/Calculate.java
@@ -1,6 +1,16 @@
 package ru.zarubin;
-
+/**
+ * //TODO add comments.
+ *
+ *@author zarubin
+ *@since 14/01/2017
+ *@version 0.1
+ *@param value строка для вывода в консоль
+ */
 public class Calculate {
+	/**
+	*@param value строка для вывода в консоль
+	*/
 	public String echo(String value) {
 		return String.format("%s %s %s", value, value, value);
 	}
