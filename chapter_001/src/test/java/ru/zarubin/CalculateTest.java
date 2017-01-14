5dcef90826edd1b60769152f41b855cfd1dfbfb9
Fedor-MZarubin <hard88@inbox.ru>
2017-01-15 00:38:35 +0300

    Java-a-to-z

--- chapter_001/src/test/java/ru/zarubin/CalculateTest.java
+++ chapter_001/src/test/java/ru/zarubin/CalculateTest.java
@@ -4,7 +4,18 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+/**
+ * Test
+ *
+ *@author zarubin
+ *@since 14/01/2017
+ *@version 0.1
+ **/
+
 public class CalculateTest {
+	/**
+	*Test add.
+	*/
 	
 	@Test
 	public void whenSetStopInEchoThenReturnThreeStops() {
@@ -20,4 +31,5 @@ public class CalculateTest {
 		assertThat(result, is("null null null"));
 	}
 
+	
 }
---
+++ chapter_001/src/test/java/ru/zarubin/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * //TODO add comments.
+ *
+ *@author zarubin
+ *@since 14/01/2017
+ *@version 0.1
+ *@param value строка для вывода в консоль
+ */
+package ru.zarubin
